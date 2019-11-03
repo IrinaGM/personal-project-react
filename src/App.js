645a7647ee6ec5fbd862569bcec673525ff225c6
IrinaGM <irina.golionko@gmail.com>
2019-11-03 12:39:40 -0500

    changed location of provider and store to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import MainView from './Views/MainView';
 import './App.css';
+import {Provider} from 'react-redux';
+import {store} from './store/store';
 
 function App() {
   return (
-    <div className="App">
-      <MainView />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <MainView />
+      </div>
+    </Provider>
   );
 }
 
